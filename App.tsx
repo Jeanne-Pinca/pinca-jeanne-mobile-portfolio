8b2f9978da1aadd94445f60fea20caf4fdf5e35b
jeiyaaa <pinca.jeanne@gmail.com>
2026-01-22 15:51:02 +0800

    initial setup

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import FunctionalComponent from './src/pages/FunctionalComponent';
+import Counter from './src/pages/UseState';
+import { styles } from './src/styles/globalStyles';
+import ConditionalRendering from './src/pages/ConditionalRendering';
+import EventHandling from './src/pages/EventHandling';
+import UseEffect from './src/pages/UseEffect';
+import UseContext from './src/pages/UseContext';
+
+export default function App() {
+  return (
+    <View style={styles.mainContainer}>
+      <UseEffect/>
+      {/* <Counter></Counter>
+      <ConditionalRendering></ConditionalRendering>
+      <EventHandling></EventHandling>
+      <UseEffect></UseEffect> */}
+    </View>
+    // <UseContext></UseContext>
+  );
+}
